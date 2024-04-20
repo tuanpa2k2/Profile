1866c9ad05e55eb207cea98f4056f7c42e7f1e27
tuanpa2k2 <tuanpa2k2@gmail.com>
2024-04-20 20:51:20 +0700

    # UI Hero cơ bản

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import NavbarComp from "./Component/Navbar/NavbarComp";
+import HeroComp from "./Component/Hero/HeroComp";
 
 const App = () => {
   return (
     <div>
       <NavbarComp />
+      <HeroComp />
     </div>
   );
 };
---
+++ src/Component/Hero/HeroComp.jsx
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+import profile_hero from "../../assets/default.jpg";
+import "./HeroComp.scss";
+
+const HeroComp = () => {
+  return (
+    <div className="hero">
+      <div className="img_hero">
+        <img src={profile_hero} alt="hero" />
+      </div>
+      <h1>
+        <span>Mình tên Phí Anh Tuấn</span>, biệt danh: <strong>Snake</strong> 🐍
+      </h1>
+      <p>
+        Error dolorum neque ea similique, labore cumque necessitatibus voluptatem excepturi iure minus veritatis, porro
+        blanditiis voluptas beatae illum.
+      </p>
+    </div>
+  );
+};
+
+export default HeroComp;
