5131d3f833613769df136a80ba8fd8f2b3b3692a
tuanpa2k2 <tuanpa2k2@gmail.com>
2024-05-08 09:47:57 +0700

    # handle click active and scroll sticky navbar

--- src/Component/Navbar/NavbarComp.jsx
+++ src/Component/Navbar/NavbarComp.jsx
@@ -1,20 +1,49 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./NavbarComp.scss";
 
 const NarbarComp = () => {
+  const [menuActive, setMenuActive] = useState("");
+  const [scrolled, setScrolled] = useState(false); // scrolled add className 'sticky-header'
+  console.log("scroll: ", scrolled);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollNavbar);
+  }, []);
+
+  const handleScrollNavbar = () => {
+    const offset = window.scrollY;
+
+    if (offset > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
   return (
-    <div className="navbar">
-      <div className="logo">
+    <div className={`navbar ${scrolled ? "sticky-header" : ""}`}>
+      <div className="logo" onClick={() => setMenuActive("")}>
         <span>my cv</span>
       </div>
       <ul className="nav_menu">
-        <li>Trang chủ</li>
-        <li>Giới thiệu</li>
-        <li>Kĩ năng</li>
-        <li>Dự án</li>
-        <li>Liên hệ</li>
+        <li onClick={() => setMenuActive("home")} className={`${menuActive === "home" ? "actives" : ""}`}>
+          Trang chủ
+        </li>
+        <li onClick={() => setMenuActive("about")} className={`${menuActive === "about" ? "actives" : ""}`}>
+          Giới thiệu
+        </li>
+        <li onClick={() => setMenuActive("skill")} className={`${menuActive === "skill" ? "actives" : ""}`}>
+          Kĩ năng
+        </li>
+        <li onClick={() => setMenuActive("project")} className={`${menuActive === "project" ? "actives" : ""}`}>
+          Dự án
+        </li>
+        <li onClick={() => setMenuActive("contact")} className={`${menuActive === "contact" ? "actives" : ""}`}>
+          Liên hệ
+        </li>
       </ul>
       <div className="nav_connect">Kết nối với tôi</div>
     </div>
