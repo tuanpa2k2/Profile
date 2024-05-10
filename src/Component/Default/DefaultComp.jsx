7a4b82d437aa7638725d58eec5aa335804a77e55
tuanpa2k2 <tuanpa2k2@gmail.com>
2024-05-10 22:22:37 +0700

    # Fix css cuộn lên đầu trang khi Click

--- src/Component/Default/DefaultComp.jsx
+++ src/Component/Default/DefaultComp.jsx
@@ -5,9 +5,20 @@ import HeroComp from "../Hero/HeroComp";
 import AboutComp from "../About/AboutComp";
 import ServiceComp from "../Service/ServiceComp";
 import MyworkComp from "../Mywork/MyworkComp";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 // eslint-disable-next-line react/prop-types
 const DefaultComp = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [location]);
+
   return (
     <div>
       <NavbarComp isMenu />
--- src/Page/DefaultPage/DefaultPage.jsx
+++ src/Page/DefaultPage/DefaultPage.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useEffect } from "react";
 
 import NavbarComp from "../../Component/Navbar/NavbarComp";
 import FooterComp from "../../Component/Footer/FooterComp";
+import { useLocation } from "react-router-dom";
 
 const DefaultPage = ({ children }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [location]);
+
   return (
     <div>
       <NavbarComp />
